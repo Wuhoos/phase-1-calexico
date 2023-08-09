ec0cb015ae63a3532bc3b0a4a0508ea91b192cd5
David <wjw3298@gmail.com>
2023-08-08 20:49:24 -0400

    complete v2

--- index.js
+++ index.js
@@ -2,44 +2,46 @@ document.addEventListener("DOMContentLoaded", listMenuItems)
 
 function listMenuItems(){
     fetch('http://localhost:3000/menu')
-    .then(response => response.json())
-    .then(menu => {
-        renderMenuItem(menu)
-        showMenuDetails(menu[0])
+    .then(Response => Response.json())
+    .then(menuList => {
+        menuDisplay(menuList)
+        renderMenuDetail(menuList[0])
+        addingToCart()
     })
 }
 
-function renderMenuItem(menuData){
-    const newMenu = document.getElementById('menu-items')
-    menuData.forEach(item => {
-        const itemsList = document.createElement('span')
-        itemsList.textContent = item.name
-        newMenu.append(itemsList)
-        itemsList.addEventListener('click', e => {
-            e.preventDefault()
-            showMenuDetails(item)})
-
+function menuDisplay(menuList){
+    const mainMenu = document.querySelector('#menu-items')
+    menuList.forEach(menuItem => {
+        const menuTag = document.createElement('span')
+        menuTag.textContent = menuItem.name
+        mainMenu.append(menuTag)
+        menuTag.addEventListener('click', () => {
+            renderMenuDetail(menuItem)})
     })
 }
 
-function showMenuDetails(menuData){
-    const name = document.getElementById('dish-name')
-    const img = document.getElementById('dish-image')
-    const description = document.getElementById('dish-description')
-    const price = document.getElementById('dish-price')
-    name.textContent = menuData.name;
-    img.src = menuData.image;
-    description.textContent = menuData.description;
-    price.textContent = menuData.price;
+function renderMenuDetail(menuItem){
+    const image = document.getElementById('dish-image')
+    const name = document.querySelector('#dish-name')
+    const description = document.querySelector('#dish-description')
+    const price = document.querySelector('#dish-price')
+
+    image.src = menuItem.image
+    name.textContent = menuItem.name
+    description.textContent = menuItem.description
+    price.textContent = `$${menuItem.price}`
+}
+
+function addingToCart(){
+    const submitForm = document.getElementById('cart-form')
+    submitForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const inputValue = document.getElementById('cart-amount').value
+        const totalValue = document.getElementById('number-in-cart')
+        totalValue.textContent = parseInt(totalValue.textContent) + parseInt(inputValue)
+    })
 }
 
 
-let cartNumber = 0
-const form = document.getElementById('cart-form')
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    const cartAmount = document.getElementById('cart-amount').value
-    cartNumber = cartNumber + Number.parseInt(cartAmount)
-    const numberInCart = document.getElementById('number-in-cart')
-    numberInCart.innerHTML = cartNumber
-})
+// addingToCart()
